a8b6356206f0969f6bde98f0083e71b9667028c4
26feb97shubham <sjain01ajmer@gmail.com>
2026-09-20 22:39:06 +0530

    Header Updated

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,5 +1,5 @@
 import { AnnouncementBar } from "@/components/layout/announcement-bar";
-import { Header } from "@/components/layout/header";
+import Header from "@/components/layout/header"
 import { Footer } from "@/components/layout/footer";
 
 export default function StoreLayout({
